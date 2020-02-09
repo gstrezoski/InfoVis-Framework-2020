3480aaf4e7773960ce034c657d423e7b4480ff30
Leonard Herold <git@hdld.me>
2020-02-09 21:48:56 +0100

    Add piechart

--- app/static/js/barchart.js
+++ app/static/js/barchart.js
@@ -9,67 +9,50 @@ function get_info_on_var(variable) {
     return [label, definition]
 }
 
-var width = 1000;
+var width = 700;
 var height = 700;
 var MOUSEOVER = false;
 
 var svgContainer = d3.select("#barchart").append("svg")
 						.attr("height", height)
-						.attr("width", width);
-
-// var circle = svgContainer.append("circle")
-// 						.attr("cx", 30)
-// 						.attr("cy", 30)
-// 						.attr("r", 20);
-
-//  var rectangle = svgContainer.append("rect")
-// 							.attr("x", 10)
-// 							.attr("y", 10)
-// 							.attr("width", 50)
-// 							.attr("height", 100);
+                        .attr("width", width);
+                        
+var svgPieContainer = d3.select("#piechart").append("svg")
+ 						.attr("height", height)
+ 						.attr("width", width);
 
 
-// var ellipse = svgContainer.append("ellipse")
-// 							.attr("cx", 50)
-// 							.attr("cy", 50)
-// 							.attr("rx", 25)
-// 							.attr("ry", 10)
-// 							.attr("fill", "red")
-// 							.attr("id", "ellipse");
 
-// d3.select("#ellipse").attr("fill","green");
+var pieData = d3.map(data[0]).entries().map(x => x.value);
+console.log(pieData);
+var pie = d3.pie()(pieData);
 
+var svg = svgPieContainer, radius = Math.min(width, height) / 2, g = svg.append("g").attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
-// var line = svgContainer.append("line")
-//                          .attr("x1", 5)
-//                          .attr("y1", 5)
-//                          .attr("x2", 50)
-//                          .attr("y2", 50)
-//                          .attr("stroke-width", 2)
-//                          .attr("stroke", "blue");
+var color = d3.scaleOrdinal(['#4daf4a','#377eb8','#ff7f00','#984ea3','#e41a1c']);
 
-// var arc = d3.arc()
-//     .innerRadius(40)
-//     .outerRadius(100)
-//     .startAngle(0)
-//     .endAngle(3);
+// Generate the arcs
+var arc = d3.arc()
+        .innerRadius(0)
+        .outerRadius(radius);
 
-// svgContainer.append("path")
-// 	.attr("transform", "translate(" + 100 + "," + 100 + ")")
-//     .attr("d", arc)
-//     .attr("fill", "red")
-//     .attr("class", "arc")
-//     .on("click", function(d) {
-//     	d3.select(".arc").attr("fill","blue");
-//     });
+//Generate groups
+var arcs = g.selectAll("arc")
+        .data(pie)
+        .enter()
+        .append("g")
+        .attr("class", "arc")
 
+//Draw arc paths
+arcs.append("path")
+.attr("fill", function(d, i) {
+    return color(i);
+})
+.attr("d", arc);
 
 var chart_height = 400,
 	chart_width = 700;
 
-// Code for vertical bar chart
-// var x = d3.scaleBand().rangeRound([0, chart_width]).padding(0.1),
-//     y = d3.scaleLinear().rangeRound([chart_height, 0]);
 
 var x = d3.scaleLinear().rangeRound([0, chart_width]),
     y = d3.scaleBand().rangeRound([chart_height, 0]).padding(0.1);
@@ -78,22 +61,12 @@ var chart_group = svgContainer.append("g")
 	.attr("id", "chart_group")
     .attr("transform", "translate(" + 100 + "," + 50 + ")");
 
-// Code for vertical bar chart
-// x.domain(x_variables);
-// y.domain([0, 100]);
-
 x.domain([0, 100]);
 y.domain(x_variables);
 
 chart_group.append("g")
     .attr("transform", "translate(" + 0 + "," + chart_height + ")")
     .call(d3.axisBottom(x));
-    // Code for vertical bar chart
-    // .selectAll("text")
-    // .attr("y", 0)
-    // .attr("x", 9)
-    // .attr("transform", "rotate(90)")
-    // .style("text-anchor", "start");
 
 chart_group.append("g")
     .call(d3.axisLeft(y));
@@ -156,44 +129,5 @@ chart_group.append("text")
         .style("font-weight", "bold")               
         .style("text-anchor", "middle")
         .text("Rental statistics of " + selected_area);
-
-// Code for vertical bar chart
-// chart_group.selectAll(".bar")
-//     .data(map.entries())
-//     .enter()
-//     .append("rect")
-//     .attr("class", "bar")
-//     .attr("x", function (d) { return x(d.key)})
-//     .attr("y", function (d) { return y(d.value) })
-//     .attr("width", x.bandwidth())
-//     .attr("height", function(d) { return chart_height - y(d.value); })
-//     .on("mouseover", function(d, i) {
-//         var x_var = d.key;
-//         var value = d.value;
-//         var info = get_info_on_var(x_var);
-//         var label = info[0]
-//         var definition = info[1];
-
-//         displayTooltip("<b>Variable: </b>" + label + "<br /><b>Percentage: </b>" + 
-//             value + "%<br /><b>Explanation: </b>" + definition)
-
-//         //d3.select(this).attr("fill", "DarkOrange");
-//     })
-//     .on("mousemove", function(d, i) {
-//         var x_var = d.key;
-//         var value = d.value;
-//         var info = get_info_on_var(x_var);
-//         var label = info[0]
-//         var definition = info[1];
-
-//         displayTooltip("<b>Variable: </b>" + label + "<br /><b>Percentage: </b>" + 
-//             value + "%<br /><b>Explanation: </b>" + definition)
-
-//         //d3.select(this).attr("fill", "DarkOrange");
-//     })
-//     .on("mouseout", function(d) {
-//         hideTooltip();
-//         //d3.select(this).attr("fill", "steelblue");
-//     });
 
 
